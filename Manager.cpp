95d4aa09b8d0f94646eed5b25735adf420b96e44
Kevin Nguyen <kln4@stmarys-ca.edu>
2015-11-23 17:47:14 -0800

    Added functionality

--- Manager.cpp
+++ Manager.cpp
@@ -5,10 +5,46 @@ Project #3
 */
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <fstream>
 #include "Queue.h"
 #include "Queue.cpp"
 using namespace std;
+
+class Record {
+private:
+    int time;
+    string name;
+    int games; // number of games they want to play
+    int rank; // priority
+public:
+    // public methods for Record class
+};
+
+void read(Record arr[]) {
+    ifstream infile;
+    string filename, fline;
+    int i = 0;
+    
+    cout << "Name of file: ";
+    getline(cin, filename);
+    infile.open(filename);
+    
+    if (infile) { // check if file is "good"/not corrupted
+        while (getline(infile, fline)) { // alternative to eof flag.
+            Record temp;
+            stringstream(fline) >> temp.time >> temp.name >> temp.games >> temp.rank;
+            arr[i] = temp;
+            i++;
+        }
+    }
+    else {
+        cout << "ERROR: Cannot open file." << endl;
+    }
+    infile.close();
+    return;
+}
 
 int main() {
     
