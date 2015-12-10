e466cbf4ffe6ad0786583e992ff352eb97c0fcb2
Kevin Nguyen <kln4@stmarys-ca.edu>
2015-12-09 23:58:52 -0800

    more comments

--- Manager.cpp
+++ Manager.cpp
@@ -2,6 +2,7 @@
 Name(s): Kevin Nguyen & Robert Posada
 Date: 11/20/2015
 Project #3
+Resource Manager
 */
 
 #include <iostream>
@@ -79,135 +80,7 @@ public:
     void logEnd();
 };
 
-Manager::Manager(Qlist<Record> g,Qlist<Record> s, Qlist<Record> b) {
-    clock = 0;
-    gold = g;
-    silver = s;
-    bronze = b;
-}
-
-int Manager::get_clock() {
-    return clock;
-}
-
-void Manager::make_teams(vector<Record>& records) {
-    if (gold.getSize() < 3) {
-        for (int i = gold.getSize(); i < 3; i++) {
-            Record bot = Record(0, "BOT", 1, 1);
-            gold.push_back(bot);
-        }
-    }
-    if (silver.getSize() < 2) {
-        for (int i = silver.getSize(); i < 2; i++) {
-            Record bot = Record(0, "BOT", 1, 2);
-            silver.push_back(bot);
-        }
-    }
-    if (bronze.getSize() < 1) {
-        for (int i = bronze.getSize(); i < 1; i++) {
-            Record bot = Record(0, "BOT", 1, 3);
-            bronze.push_back(bot);
-        }
-    }
-    
-    team1.push_back(gold[0]);
-    team2.push_back(gold[1]);
-    team1.push_back(gold[2]);
-    team2.push_back(silver[0]);
-    team2.push_back(silver[1]);
-    team1.push_back(bronze[0]);
-
-    
-    // Should put code from here down in separate function(s)
-    vector<Record> teams = team1;
-    teams.insert(teams.end(), team2.begin(), team2.end());
-    for (int i = 0; i < teams.size() - 1; i++) {
-        for (int j = 1; j < teams.size(); j++) {
-            if (teams.at(j).get_time() < teams.at(j - 1).get_time()) {
-                Record temp = teams.at(j);
-                teams.at(j) = teams.at(j - 1);
-                teams.at(j - 1) = temp;
-            }
-        }
-    }
-    if (clock > gameStart) {
-        gameStart = clock + 1;
-    }
-    else {
-        gameStart = teams.at(teams.size()-1).get_time();
-    }
-    for (Record r : teams) {
-        if (r.time > gameEnd && r.time <= gameStart) {
-            write(r, 1);
-        }
-    }
-    clock = gameEnd = gameStart + 10;
-    
-    for (Record r : team1) {
-        write(r, 2);
-    }
-    for (Record r : team2) {
-        write(r, 2);
-    }
-    for (int i = 0; i < 3; i++) {
-        gold.pop_front();
-    }
-    for (int i = 0; i < 2; i++) {
-        silver.pop_front();
-    }
-    bronze.pop_front();
-    return;
-}
-
-void Manager::write(Record rec, int option) {
-    ofstream outfile;
-    string fline;
-    outfile.open(LOGFILE, ios::app);
-    if (outfile) {
-        switch (option) {
-            case 1:
-                outfile << setfill('0') << setw(3) << rec.get_time() << " -- " << rec.get_name() << " entered queue for " << rec.get_games() << " games" << endl;
-                break;
-            case 2:
-                outfile << setfill('0') << setw(3) << gameStart << " -- " << rec.get_name() << " started game" << endl;
-                break;
-            case 3:
-                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " ended game" << endl;
-                break;
-            case 4:
-                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " was promoted to rank " << rec.get_rank() << endl;
-                break;
-            case 5:
-                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " was demoted to rank " << rec.get_rank() << endl;
-                break;
-            case 6:
-                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " left queue" << endl;
-                break;
-            case 7:
-                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " re-entered queue for " << rec.get_games() << " games" << endl;
-        }
-    }
-    else {
-        cout << "ERROR: Cannot open file." << endl;
-    }
-    outfile.close();
-    return;
-}
-
-void Manager::logEnd() {
-    ofstream outfile;
-    string fline;
-    outfile.open(LOGFILE, ios::app);
-    if (outfile) {
-        outfile << setfill('0') << setw(3) << gameEnd + 1 << " -- All requests have been fulfilled." << endl;
-    }
-    else {
-        cout << "ERROR: Cannot open file." << endl;
-    }
-    outfile.close();
-    return;
-}
-
+// Definition of Record class below
 Record::Record() {
     time = 0;
     name = "";
@@ -236,6 +109,145 @@ int Record::get_games() {
 
 int Record::get_rank() {
     return rank;
+}
+
+// Definition of Manager class and other methods below
+Manager::Manager(Qlist<Record> g,Qlist<Record> s, Qlist<Record> b) {
+    clock = 0;
+    gold = g;
+    silver = s;
+    bronze = b;
+}
+
+int Manager::get_clock() {
+    return clock;
+}
+
+void Manager::make_teams(vector<Record>& records) {
+    // Checks to see if either gold, silver, or bronze queue has too few players to complete a team
+    // If true, BOT/fake players are inserted into game, which is quite similar to actual online gameplay
+    if (gold.getSize() < 3) {
+        for (int i = gold.getSize(); i < 3; i++) {
+            Record bot = Record(0, "BOT", 1, 1);
+            gold.push_back(bot);
+        }
+    }
+    if (silver.getSize() < 2) {
+        for (int i = silver.getSize(); i < 2; i++) {
+            Record bot = Record(0, "BOT", 1, 2);
+            silver.push_back(bot);
+        }
+    }
+    if (bronze.getSize() < 1) {
+        for (int i = bronze.getSize(); i < 1; i++) {
+            Record bot = Record(0, "BOT", 1, 3);
+            bronze.push_back(bot);
+        }
+    }
+    
+    // Makes a "fair" team w/ team 1: 2 golds, 1 bronze & team2: 1 gold, 2 silvers
+    team1.push_back(gold[0]);
+    team2.push_back(gold[1]);
+    team1.push_back(gold[2]);
+    team2.push_back(silver[0]);
+    team2.push_back(silver[1]);
+    team1.push_back(bronze[0]);
+
+    
+    // Should put code from here down in separate function(s)
+    vector<Record> teams = team1;
+    teams.insert(teams.end(), team2.begin(), team2.end());
+    for (int i = 0; i < teams.size() - 1; i++) {
+        for (int j = 1; j < teams.size(); j++) {
+            if (teams.at(j).get_time() < teams.at(j - 1).get_time()) {
+                Record temp = teams.at(j);
+                teams.at(j) = teams.at(j - 1);
+                teams.at(j - 1) = temp;
+            }
+        }
+    }
+    
+    if (clock > gameStart) {
+        gameStart = clock + 1;
+    }
+    else {
+        gameStart = teams.at(teams.size()-1).get_time();
+    }
+    for (Record r : teams) {
+        if (r.time > gameEnd && r.time <= gameStart) {
+            write(r, 1);
+        }
+    }
+    clock = gameEnd = gameStart + 10;
+    
+    for (Record r : team1) {
+        write(r, 2);
+    }
+    for (Record r : team2) {
+        write(r, 2);
+    }
+    // Removes the players from their respective queues that just played
+    for (int i = 0; i < 3; i++) {
+        gold.pop_front();
+    }
+    for (int i = 0; i < 2; i++) {
+        silver.pop_front();
+    }
+    bronze.pop_front();
+    
+    return;
+}
+
+// Logging function to document all operations
+void Manager::write(Record rec, int option) {
+    ofstream outfile;
+    string fline;
+    outfile.open(LOGFILE, ios::app); // need ios::app to append to the existing file, not overwrite
+    // if the file is not "corrupted"
+    if (outfile) {
+        switch (option) {
+            case 1:
+                outfile << setfill('0') << setw(3) << rec.get_time() << " -- " << rec.get_name() << " entered queue for " << rec.get_games() << " games" << endl;
+                break;
+            case 2:
+                outfile << setfill('0') << setw(3) << gameStart << " -- " << rec.get_name() << " started game" << endl;
+                break;
+            case 3:
+                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " ended game" << endl;
+                break;
+            case 4:
+                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " was promoted to rank " << rec.get_rank() << endl;
+                break;
+            case 5:
+                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " was demoted to rank " << rec.get_rank() << endl;
+                break;
+            case 6:
+                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " left queue" << endl;
+                break;
+            case 7:
+                outfile << setfill('0') << setw(3) << gameEnd << " -- " << rec.get_name() << " re-entered queue for " << rec.get_games() << " games" << endl;
+        }
+    }
+    else {
+        cout << "ERROR: Cannot open file." << endl;
+    }
+    // close file
+    outfile.close();
+    return;
+}
+
+void Manager::logEnd() {
+    ofstream outfile;
+    string fline;
+    outfile.open(LOGFILE, ios::app);
+    if (outfile) {
+        outfile << setfill('0') << setw(3) << gameEnd + 1 << " -- All requests have been fulfilled." << endl;
+    }
+    else {
+        cout << "ERROR: Cannot open file." << endl;
+    }
+    outfile.close();
+    return;
 }
 
 void read(vector <Record>& records) {
@@ -315,6 +327,7 @@ void Manager::game(int win, vector <Record>& team1, vector <Record>& team2) {
     return;
 }
 
+// Logs external requests while in a game
 void Manager::updateLog(vector<Record>& records) {
     for (Record r : records) {
         if (r.time > gameStart && r.time < gameEnd) {
@@ -324,6 +337,7 @@ void Manager::updateLog(vector<Record>& records) {
     return;
 }
 
+// Updates the times that players re-enter queue if they have more games
 void Manager::updateTime(vector<Record>& records) {
     for (Record r: records) {
         r.time = gameEnd;
@@ -331,6 +345,7 @@ void Manager::updateTime(vector<Record>& records) {
     return;
 }
 
+// Reassigns those who just played to the back of their respective queues if they have more games
 void Manager::reassign() {
     for (Record r : team1) {
         write(r, 3);
